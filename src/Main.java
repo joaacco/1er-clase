592266a7c154d5b42d1d34a9780fe24ba654ff5a
joaacco <joaquin.lcejas@davinci.edu.ar>
2024-09-09 10:11:06 -0300

    agrego saludo

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
+import javax.swing.JOptionPane;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		
+		JOptionPane.showMessageDialog(null, "hola mundo");
 
 	}
 
